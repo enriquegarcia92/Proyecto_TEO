83130aec32d609bb28d6e851c03394d592b8f80d
enriquegarcia92 <00093619@uca.edu.sv>
2023-11-21 16:38:04 -0600

    gramatica

--- proyectoenv/main.py
+++ proyectoenv/main.py
@@ -3,12 +3,6 @@
 # ------------------------------------------------------------
 import ply.lex as lex
 
-S = 0
-S2 = 1
-T = 2
-T2 = 3
-F = 4
-
 # List of token names. This is always required
 tokens = (
     'NUMBER',
@@ -28,11 +22,10 @@ tokens = (
     'comentario_bloque',
     'cadena',
     'coma',
-    'hash_include',
-    'preprocessor_directive',
+    'eof',
     'int',
     'float',
-    'greater_than',  # Nuevo token para '>'
+    'INCLUDE',  # Added for #include
 )
 
 # Regular expression rules for simple tokens
@@ -47,7 +40,14 @@ t_finBloque = r'\}'
 t_finInstruccion = r'\;'
 t_asignacion = r'\='
 t_coma = r'\,'
-t_hash_include = r'\#include'
+t_eof = r'\$'
+
+
+# Preprocessor directive rule
+def t_INCLUDE(t):
+    r'\#include\s*\<[^\>]+\>'
+    return t
+
 
 def t_int(t):
     r'(int)'
@@ -77,7 +77,7 @@ t_ignore = ' \t'
 
 
 def t_keyword(t):
-    r'(char|return|if|else|do|while|for|void)'
+    r'(char)|(return)|(if)|(else)|(do)|(while)|(for)|(void)'
     return t
 
 
@@ -87,7 +87,7 @@ def t_identificador(t):
 
 
 def t_cadena(t):
-    r'\"[^\"]*\"'
+    r'\".*\"'
     return t
 
 
@@ -101,65 +101,28 @@ def t_comentario_bloque(t):
     # return t
 
 
-def t_preprocessor_directive(t):
-    r'\#.*'
-    return t
-
-
-def t_greater_than(t):
-    r'>'
-    return t
-
-
 # Error handling rule
 def t_error(t):
     print(f"Illegal character '{t.value[0]}' at line {t.lineno}, position {t.lexpos}")
     t.lexer.skip(1)
 
+
+# Build the lexer
 lexer = lex.lex()
 
-# Agregamos el código C a la entrada del lexer
-code = """
-#include <stdio.h>
+lexer.input("""
+    #include <stdio.h>
+    """)
 
-int suma(int a, int b) {
-    return a + b;
-}
-
-void imprimir_mayor(int x, int y) {
-    if (x > y) {
-        printf("El número %d es mayor que %d\n", x, y);
-    } else {
-        printf("El número %d es menor o igual que %d\n", x, y);
-    }
-}
-
-int main() {
-    int numero_entero = 10;
-    char caracter = 'A';
-    float numero_flotante = 5.5;
-
-    // Llamando a la función suma
-    int resultado = suma(numero_entero, 20);
-    printf("El resultado de la suma es: %d\n", resultado);
-
-    // Llamando a la funcion imprimir_mayor
-    imprimir_mayor(8, numero_entero);
-
-    return 0;
-}
-"""
-
-lexer.input(code)
-
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok:
-        break  # No more input
+# Print the tokens produced by the lexer
+for tok in lexer:
     print(tok)
 
-
+S = 0
+S2 = 1
+T = 2
+T2 = 3
+F = 4
 TT = 1
 D = 2
 tabla = [[S, 'identificador', None],
@@ -181,14 +144,13 @@ tabla = [[S, 'identificador', None],
 
 stack = ['eof', 0]
 
-# Build the lexer
-lexer = lex.lex()
-
 
 def miParser():
     # f = open('fuente.c','r')
     # lexer.input(f.read())
     code = """
+    #include <stdio.h>
+
     int suma(int a, int b) {
         return a + b;
     }
@@ -216,7 +178,9 @@ def miParser():
         return 0;
     }
     """
-    lexer.input(code)
+    lexer.input("""
+        #include <stdio.h>
+        int a, b, c;$""")
 
     tok = lexer.token()
     x = stack[-1]  # primer elemento de der a izq
@@ -265,4 +229,111 @@ def agregar_pila(produccion):
         if elemento != 'vacia':  # la vacía no la inserta
             stack.append(elemento)
 
+
 miParser()
+
+"""
+1. Program -> IncludeDeclaration MainFunction
+
+2. IncludeDeclaration -> INCLUDE STRING finInstruccion
+
+3. MainFunction -> 'int' 'main' '(' ')' '{' Declaration* Statement* 'return' NUMBER ';' '}'
+
+4. Declaration -> Type IdentifierList finInstruccion
+
+5. Type -> 'int' | 'char' | 'float'
+
+6. IdentifierList -> 'identificador' IdentifierList'
+
+7. IdentifierList' -> ',' 'identificador' IdentifierList' | ε
+
+8. Statement -> 'identificador' '=' Expression finInstruccion
+              | 'if' '(' Expression ')' '{' Statement* '}' 'else' '{' Statement* '}'
+              | 'printf' '(' STRING ',' ExpressionList ')' finInstruccion
+              | 'return' Expression finInstruccion
+              | 'void' 'imprimir_mayor' '(' Expression ',' Expression ')' '{' Statement* '}'
+              | 'while' '(' Expression ')' '{' Statement* '}'
+
+9. ExpressionList -> Expression ExpressionList'
+
+10. ExpressionList' -> ',' Expression ExpressionList' | ε
+
+11. Expression -> SimpleExpression ComparisonOp SimpleExpression | SimpleExpression
+
+12. ComparisonOp -> '>' | '<' | '>=' | '<=' | '==' | '!='
+
+13. SimpleExpression -> Term SimpleExpression'
+
+14. SimpleExpression' -> '+' Term SimpleExpression' | '-' Term SimpleExpression' | ε
+
+15. Term -> Factor Term'
+
+16. Term' -> '*' Factor Term' | '/' Factor Term' | ε
+
+17. Factor -> 'identificador' | NUMBER | '(' Expression ')' | 'printf' | STRING
+
+18. StatementList -> Statement StatementList' | ε
+
+19. StatementList' -> Statement StatementList' | ε
+
+FIRST(Program) = {INCLUDE, 'int'}
+FIRST(IncludeDeclaration) = {INCLUDE}
+FIRST(MainFunction) = {'int'}
+FIRST(Declaration) = {'int', 'char', 'float'}
+FIRST(Type) = {'int', 'char', 'float'}
+FIRST(IdentifierList) = {'identificador'}
+FIRST(IdentifierList') = {',', ε}
+FIRST(Statement) = {'identificador', 'if', 'printf', 'return', 'void', 'while'}
+FIRST(ExpressionList) = {'identificador', NUMBER, '(', 'printf', STRING, ε}
+FIRST(ExpressionList') = {',', ε}
+FIRST(Expression) = {'identificador', NUMBER, '(', 'printf', STRING}
+FIRST(ComparisonOp) = {'>', '<', '>=', '<=', '==', '!='}
+FIRST(SimpleExpression) = {'identificador', NUMBER, '(', 'printf', STRING}
+FIRST(SimpleExpression') = {'+', '-', ε}
+FIRST(Term) = {'identificador', NUMBER, '(', 'printf', STRING}
+FIRST(Term') = {'*', '/', ε}
+FIRST(Factor) = {'identificador', NUMBER, '(', 'printf', STRING}
+
+FOLLOW Sets:
+plaintext
+Copy code
+FOLLOW(Program) = {eof}
+FOLLOW(IncludeDeclaration) = {INCLUDE, 'int'}
+FOLLOW(MainFunction) = {eof}
+FOLLOW(Declaration) = {INCLUDE, 'int', 'char', 'float', '}', 'if', 'printf', 'return', 'void', 'while'}
+FOLLOW(Type) = {'identificador'}
+FOLLOW(IdentifierList) = {';', ')'}
+FOLLOW(IdentifierList') = {';', ')'}
+FOLLOW(Statement) = {'identificador', 'if', 'printf', 'return', 'void', 'while', '}', eof}
+FOLLOW(ExpressionList) = {')'}
+FOLLOW(ExpressionList') = {')'}
+FOLLOW(Expression) = {')', ';', '==', '!=', '>', '<', '>=', '<='}
+FOLLOW(ComparisonOp) = {'identificador', NUMBER, '(', 'printf', STRING}
+FOLLOW(SimpleExpression) = {')', ';', '==', '!=', '>', '<', '>=', '<=', '+', '-', 'identificador', NUMBER, '(', 'printf', STRING}
+FOLLOW(SimpleExpression') = {')', ';', '==', '!=', '>', '<', '>=', '<='}
+FOLLOW(Term) = {')', ';', '==', '!=', '>', '<', '>=', '<=', '+', '-', 'identificador', NUMBER, '(', 'printf', STRING}
+FOLLOW(Term') = {')', ';', '==', '!=', '>', '<', '>=', '<=', '+', '-'}
+FOLLOW(Factor) = {'*', '/', ')', ';', '==', '!=', '>', '<', '>=', '<=', '+', '-', 'identificador', NUMBER, '(', 'printf', STRING}
+LL(1) Parsing Table:
+
+|        | INCLUDE | int | char | float | identificador | NUMBER | ( | ) | { | } | ; | , | if | else | printf | return | void | while | + | - | * | / | < | > | <= | >= | == | != | STRING | eof |
+|--------|---------|-----|------|-------|----------------|--------|---|---|---|---|---|---|----|------|--------|--------|------|-------|---|---|---|---|---|---|----|----|----|----|--------|-----|
+| Program        |         | S   |     |       | S              |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| IncludeDeclaration | INCLUDE |     |     |       |                |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| MainFunction    |         |     |     |       |                |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| Declaration    |         | S   | S   | S     |                |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| Type           |         | S   | S   | S     |                |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| IdentifierList |         | S   |     |       | S              |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| IdentifierList'|   ε     |     |     |       |                |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| Statement      |         | S   |     |       | S              |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| ExpressionList |         | S   |     |       | S              |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| ExpressionList'|   ε     |     |     |       |                |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| Expression     |         | S   |     |       | S              | S      | S |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| ComparisonOp   |         |     |     |       |                |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| SimpleExpression|        | S   |     |       | S              | S      | S |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| SimpleExpression'|   ε  |     |     |       |                |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| Term           |         | S   |     |       | S              | S      | S |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| Term'          |   ε     |     |     |       |                |        |   |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+| Factor         |         | S   |     |       | S              | S      | S |   |   |   |   |   |    |      |        |        |      |        |   |   |   |   |   |   |    |    |    |    |        |     |
+
+"""
