3127011d95990f546f740542ef2624efaeb90abb
enriquegarcia92 <00093619@uca.edu.sv>
2023-12-02 18:01:36 -0600

    tree

---
+++ proyectoenv/nonTerminal.py
@@ -0,0 +1,44 @@
+S = 0
+#Manejo de librerias
+LIB = 1
+LIB2 = 2
+LIBEND = 3
+#Uso de funciones
+FUNCUSE = 4
+#Manejo de apertura de funciones
+OPENNUMFUN = 5
+OPENCHARFUN = 6
+OPENVOID = 7
+#Manejo de parameteso
+ADDPARAM = 8
+PARAM = 9
+#Manejo de cuerpo
+FBODY = 10
+#Manejo de retornos de funciones o saingaciones
+CHARFRETURN = 11
+VOIDRETURN = 12
+NUMRETURN = 13
+RETURNCHAR = 14
+RETURNVOID = 15
+RETURNNUM = 16
+#Manejo de operaciones aritmeticas
+ARITMETIC = 17
+#Manejo de condiciones logicas
+CONDITIONSHANDLER = 18
+#impresion de datos
+PRINTCONT = 19
+#Manejo interno de la condicion logica
+LOGICSIMBOLS = 20
+NOTHANDLER = 21
+ELSEXTENTION = 22
+#Manejo de grupos aritmeticos
+GHANDLER = 23
+#Manejo de tipos de variables
+THANDLER = 24
+#Manejo de numericos
+FLOATN = 25
+DECIMAL = 26
+#Uso de parametros
+USEPARAM = 27
+#Manejo de variables vacias
+EMPTYVAR = 28
